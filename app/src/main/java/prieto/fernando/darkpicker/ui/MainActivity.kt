74531022885d13be7b4eef2155aba8f3e1aeb93d
fernando <fernando.moyano@nice.agency>
2019-10-14 13:57:26 +0100

    Deleted SeekBar lib and refactor

--- app/src/main/java/prieto/fernando/darkpicker/ui/MainActivity.kt
+++ app/src/main/java/prieto/fernando/darkpicker/ui/MainActivity.kt
@@ -2,24 +2,27 @@ package prieto.fernando.darkpicker.ui
 
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
-import com.divyanshu.colorseekbar.ColorSeekBar
-import kotlinx.android.synthetic.main.main_activity.*
 import prieto.fernando.darkpicker.R
 import prieto.fernando.darkpicker.model.Theme
 import prieto.fernando.darkpicker.presentation.MainViewModel
 import prieto.fernando.darkpicker.util.ThemeProvider
+import prieto.fernando.darkpicker.widget.ColorSeekBar
 import prieto.fernando.darkpicker.widget.ThemeMode
 import javax.inject.Inject
+import kotlinx.android.synthetic.main.main_activity.color_seek_bar as colorSeekbar
+import kotlinx.android.synthetic.main.main_activity.theme_selected as themeSelected
 
 class MainActivity : BaseActivity<MainViewModel>() {
     @Inject
     protected lateinit var themeProvider: ThemeProvider
 
     private val themes = mutableListOf<Theme>()
+    private var seekBarDragged = false
 
     override fun onResume() {
         super.onResume()
         viewModel.inputs.applyTheme(ThemeMode.DARK)
+        seekBarDragged = false
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,16 +32,13 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
         setContentView(R.layout.main_activity)
         prepareThemeData()
-        theme_selected.setTheme(themes[1])
+        themeSelected.setTheme(themes[1])
 
-        color_seek_bar.setOnColorChangeListener(object : ColorSeekBar.OnColorChangeListener {
+        colorSeekbar.setOnColorChangeListener(object : ColorSeekBar.OnColorChangeListener {
             override fun onColorChangeListener(color: Int) {
                 val hexadecimalColour = getHexadecimalColour(color)
                 themeProvider.setSelectedColour(hexadecimalColour)
-
-                theme_selected.postDelayed({
-                    this@MainActivity.recreate()
-                }, 1000)
+                this@MainActivity.recreate()
             }
         })
     }
