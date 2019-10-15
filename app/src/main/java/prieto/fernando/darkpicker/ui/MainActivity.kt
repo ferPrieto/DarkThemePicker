0706784a58d535cd64cf1fdd8fcb404f177a6639
fernando <fernando.moyano@nice.agency>
2019-10-15 15:22:20 +0100

    Floating button click added

--- app/src/main/java/prieto/fernando/darkpicker/ui/MainActivity.kt
+++ app/src/main/java/prieto/fernando/darkpicker/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package prieto.fernando.darkpicker.ui
 import android.os.Bundle
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.TranslateAnimation
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
 import prieto.fernando.darkpicker.R
 import prieto.fernando.darkpicker.model.Theme
@@ -25,10 +26,58 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
     override fun onResume() {
         super.onResume()
-        viewModel.inputs.applyTheme(ThemeMode.DARK)
         seekBarDragged = false
         animateFloatingButton()
+        setupInputListeners()
+        setupOutputListeners()
     }
+
+    private fun setupOutputListeners() {
+        viewModel.outputs.currentThemeModeRetrieved()
+            .subscribe { themeMode ->
+                setTheme(themeMode)
+                setFloatingActionButtonIcon(themeMode)
+            }.also { subscriptions.add(it) }
+    }
+
+    private fun setTheme(themeMode: ThemeMode) {
+        when (themeMode) {
+            ThemeMode.DARK -> viewModel.inputs.applyTheme(ThemeMode.LIGHT)
+            else -> viewModel.inputs.applyTheme(ThemeMode.DARK)
+        }
+    }
+
+    private fun setFloatingActionButtonIcon(themeMode: ThemeMode) {
+        when (themeMode) {
+            ThemeMode.LIGHT -> floatingActionButton.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.ic_dark
+                )
+            )
+            ThemeMode.DARK -> floatingActionButton.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.ic_light
+                )
+            )
+
+        }
+    }
+
+    private fun setupInputListeners() {
+        colorSeekBar.setOnColorChangeListener(object : ColorSeekBar.OnColorChangeListener {
+            override fun onColorChangeListener(color: Int) {
+                val hexadecimalColour = getHexadecimalColour(color)
+                themeProvider.setSelectedColour(hexadecimalColour)
+                this@MainActivity.recreate()
+            }
+        })
+        floatingActionButton.setOnClickListener {
+            viewModel.getCurrentTheme()
+        }
+    }
+
 
     private fun animateFloatingButton() {
         val animation = TranslateAnimation(0f, 0f, 500f, 0f)
@@ -41,19 +90,12 @@ class MainActivity : BaseActivity<MainViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel.inputs.initTheme()
         setTheme(themeProvider.getSelectedStyle())
 
         setContentView(R.layout.main_activity)
         prepareThemeData()
         themeSelected.setTheme(themes[1])
-
-        colorSeekBar.setOnColorChangeListener(object : ColorSeekBar.OnColorChangeListener {
-            override fun onColorChangeListener(color: Int) {
-                val hexadecimalColour = getHexadecimalColour(color)
-                themeProvider.setSelectedColour(hexadecimalColour)
-                this@MainActivity.recreate()
-            }
-        })
     }
 
     private fun getHexadecimalColour(colour: Int) = String.format("#%06X", 0xFFFFFF and colour)
--- app/src/main/java/prieto/fernando/darkpicker/widget/ThemeApplier.kt
+++ app/src/main/java/prieto/fernando/darkpicker/widget/ThemeApplier.kt
@@ -2,11 +2,14 @@ package prieto.fernando.darkpicker.widget
 
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.BuildCompat
+import io.reactivex.Single
 import javax.inject.Inject
 
-class ThemeApplier @Inject constructor(){
-    fun applyTheme(themeMode: ThemeMode) {
-        when (themeMode) {
+class ThemeApplier @Inject constructor() {
+    private var selectedTheme = ThemeMode.DEFAULT
+
+    fun initTheme() {
+        when (selectedTheme) {
             ThemeMode.LIGHT -> {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
@@ -21,10 +24,34 @@ class ThemeApplier @Inject constructor(){
                 }
             }
         }
+
     }
+
+    fun applyTheme(themeMode: ThemeMode) {
+        when (themeMode) {
+            ThemeMode.LIGHT -> {
+                selectedTheme = ThemeMode.LIGHT
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            ThemeMode.DARK -> {
+                selectedTheme = ThemeMode.DARK
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else -> {
+                selectedTheme = ThemeMode.DEFAULT
+                if (BuildCompat.isAtLeastQ()) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                }
+            }
+        }
+    }
+
+    fun getCurrentMode() = Single.just(selectedTheme)
 }
 
-enum class ThemeMode{
+enum class ThemeMode {
     LIGHT,
     DARK,
     DEFAULT
