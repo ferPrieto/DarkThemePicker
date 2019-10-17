521e65c090dff665132972a562c04f5aebb33d53
fernando <fernando.moyano@nice.agency>
2019-10-17 08:44:39 +0100

    Statusbar and Image fixes

--- app/src/main/java/prieto/fernando/darkpicker/ui/MainActivity.kt
+++ app/src/main/java/prieto/fernando/darkpicker/ui/MainActivity.kt
@@ -1,10 +1,18 @@
 package prieto.fernando.darkpicker.ui
 
+import android.content.Context
+import android.graphics.PorterDuff
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.WindowManager
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.TranslateAnimation
+import androidx.annotation.AttrRes
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.default_toolbar.*
+import kotlinx.android.synthetic.main.draw_layout.*
 import prieto.fernando.darkpicker.R
 import prieto.fernando.darkpicker.model.Theme
 import prieto.fernando.darkpicker.presentation.MainViewModel
@@ -15,6 +23,7 @@ import prieto.fernando.darkpicker.widget.ThemeMode
 import javax.inject.Inject
 import kotlinx.android.synthetic.main.main_activity.color_seek_bar as colorSeekBar
 import kotlinx.android.synthetic.main.main_activity.fab as floatingActionButton
+
 
 class MainActivity : BaseActivity<MainViewModel>() {
     @Inject
@@ -33,10 +42,42 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
         setContentView(R.layout.main_activity)
         prepareThemeData()
-        setFloatingActionButtonIcon(themeApplier.getCurrentMode().blockingGet())
+        val currentThemeMode = themeApplier.getCurrentMode().blockingGet()
+        setFloatingActionButtonIcon(currentThemeMode)
+        setupToolbar(currentThemeMode)
+        setupStatusbar(currentThemeMode)
+        setupDrawCardImage(currentThemeMode)
     }
 
-    override fun onResume() {
+    private fun setupToolbar(currentThemeMode: ThemeMode) {
+        if (currentThemeMode == ThemeMode.DARK) {
+            default_toolbar.background =
+                ColorDrawable(ContextCompat.getColor(this, R.color.darkToolbar))
+            val colorPrimary = themeColor(R.attr.colorPrimary)
+            default_toolbar.setTitleTextColor(colorPrimary)
+        }
+    }
+
+    private fun setupStatusbar(currentThemeMode: ThemeMode) {
+        val window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = if (currentThemeMode == ThemeMode.DARK) {
+            resources.getColor(R.color.darkStatusBar)
+        }else{
+            themeColor(R.attr.colorPrimaryDark)
+        }
+    }
+
+    private fun setupDrawCardImage(currentThemeMode: ThemeMode) {
+        if (currentThemeMode == ThemeMode.DARK) {
+            val colorPrimary = themeColor(R.attr.colorPrimary)
+            draw_media_image.setColorFilter(colorPrimary, PorterDuff.Mode.SRC_IN)
+        }else{
+            draw_media_image.setColorFilter(ContextCompat.getColor(this, android.R.color.darker_gray), PorterDuff.Mode.SRC_IN)
+        }
+    }
+
+        override fun onResume() {
         super.onResume()
         animateFloatingButton()
         setupInputListeners()
@@ -102,5 +143,11 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
     override val viewModel: MainViewModel by lazy {
         ViewModelProviders.of(this, vmFactory).get(MainViewModel::class.java)
+    }
+
+    fun Context.themeColor(@AttrRes attrRes: Int): Int {
+        val typedValue = TypedValue()
+        theme.resolveAttribute(attrRes, typedValue, true)
+        return typedValue.data
     }
 }
